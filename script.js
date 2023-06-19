9c54240d082346f916695f14d8847f8200440c8e
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-06-19 21:40:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
-    //Write your code here
-
+const form = document.getElementById("form1");
+const firstName = form.fname.value;
+const lastName = form.lname.value;
+alert( firstName + " " + lastName);
 }
